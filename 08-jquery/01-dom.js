8cfe45a106c2f91bff779b48c9a79c6eb8c7871c
vamsimylaPDX <vmyla@pdx.edu>
2024-03-09 12:18:19 -0800

    latest update

--- 08-jquery/01-dom.js
+++ 08-jquery/01-dom.js
@@ -1,45 +1,11 @@
 $(document).ready(() => {
-  const url = "https://anapioficeandfire.com/api/books/";
+  $("#copy").on("click", (event) => {
+    console.log("Click event", event);
+    $("#output1").text($("#userInput1").val());
+  });
 
-  // jQuery-consistent way to remove padding
-  $("#books").css("padding-left", 0);
-
-  const addBookToDOM = (item) => {
-    console.log(item.name);
-
-    $("#books").append(
-      $("<div>")
-        .css({
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginTop: "20px",
-        })
-        .append($("<h2>").text(item.name))
-        .append($("<p>").text(item.authors.join(", "))) // Assuming there could be multiple authors
-        .append($("<p>").text(item.released.substr(0, 4)))
-        .append($("<p>").text(item.numberOfPages + " pages")) // Corrected to concatenate the number of pages
-    );
-  };
-
-  const fetchData = (url) => {
-    $.ajax({
-      type: "GET",
-      url: url,
-      success: (data) => {
-        data.forEach(addBookToDOM); // Simplified by passing the function directly
-      },
-      error: (error) => {
-        console.error(error);
-        $("#books").append(
-          $("<div>").text("An error occurred. Please try again.")
-        );
-      },
-      complete: () => {
-        $("#loading").remove(); // jQuery to remove the loading element
-      },
-    });
-  };
-
-  fetchData(url);
+  $("#userInput2").on("input", (event) => {
+    console.log("Input event", event);
+    $("#output2").text($("#userInput2").val());
+  });
 });
