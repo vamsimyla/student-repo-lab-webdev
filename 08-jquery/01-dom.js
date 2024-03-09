52336ddb1d2ef500e29a70ae6bb2601e5e31169d
vamsimylaPDX <vmyla@pdx.edu>
2024-03-09 12:28:38 -0800

    lab latest updates

--- 08-jquery/01-dom.js
+++ 08-jquery/01-dom.js
@@ -1,11 +1,15 @@
 $(document).ready(() => {
+  // Exercise #1:
+  // When the user clicks the 'copy' button, copy the user input to the output area
+
   $("#copy").on("click", (event) => {
-    console.log("Click event", event);
     $("#output1").text($("#userInput1").val());
   });
 
+  // Exercise #2:
+  // When the user enters input text, copy the user input to the output area
+
   $("#userInput2").on("input", (event) => {
-    console.log("Input event", event);
     $("#output2").text($("#userInput2").val());
   });
 });
